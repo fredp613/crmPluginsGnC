93f1d37f30ec7efa24d72e01e7ce3811d44c8e99
Frederick Pearson <fredp613@gmail.com>
2015-10-05 17:35:07 -0400

    first commit

---
+++ CrmGC/Plugins/AutoNumbering.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmGC.Plugins
+{
+    using System;
+    using System.ServiceModel;
+    using Microsoft.Xrm.Sdk;
+    using Microsoft.Xrm.Sdk.Client;
+    using Microsoft.Xrm.Sdk.Query;
+
+    class AutoNumbering
+    {
+
+        public Entity entity;       
+        public IOrganizationService service;
+
+        public AutoNumbering(Entity ent, IOrganizationService serv)
+        {
+            entity = ent;
+            service = serv;
+            
+        }
+        public string getAutoNumber() {
+  
+            String autoNumber = "";
+            if (entity.LogicalName == "gcbase_fundingcase") {
+                var fundEntity = service.Retrieve("gcbase_fundcentre", entity.GetAttributeValue<EntityReference>("gcbase_program").Id, new ColumnSet("gcbase_sapfundcentre"));
+                var sapEntity = service.Retrieve("gcbase_sapfundcentre", fundEntity.GetAttributeValue<EntityReference>("gcbase_sapfundcentre").Id, new ColumnSet("gcbase_name"));
+                autoNumber = generateAutoNumber(sapEntity.GetAttributeValue<String>("gcbase_name").ToString(), getLastNumberFromEntity());
+            }
+            if (entity.LogicalName == "gcbase_client") {
+                autoNumber = generateAutoNumber("", getLastNumberFromEntity());
+            }
+            return autoNumber;
+        }
+
+        private string getLastNumberFromEntity() {
+            string entityName = entity.LogicalName;
+            QueryExpression lastAutoNumberOfEntity = new QueryExpression
+            {
+                EntityName = entityName,
+                ColumnSet = new ColumnSet("gcbase_name"),                                             
+            };
+            lastAutoNumberOfEntity.Orders.Add(new OrderExpression("gcbase_name", OrderType.Descending));
+            lastAutoNumberOfEntity.PageInfo.Count = 1;
+            lastAutoNumberOfEntity.PageInfo.PageNumber = 1;
+            var test = service.RetrieveMultiple(lastAutoNumberOfEntity).Entities.FirstOrDefault();
+            if (test != null) {
+                return test.GetAttributeValue<String>("gcbase_name");
+            }
+            return "00000";
+        }
+
+        private string generateAutoNumber(string prefix, string lastNumber) {           
+            String autoNumber = "";
+            long numberForAppending = 0;
+
+            var indexEndOfPrefix = lastNumber.IndexOf("-");
+            var lastNumberWithoutPrefix = lastNumber.Substring(indexEndOfPrefix + 1);
+            string test = lastNumberWithoutPrefix.TrimStart('0');
+            int lastNumberLen = test.Length;
+            //FaultException ex1 = new faultexception();
+            //throw new invalidPluginExecutionException(test, ex1);
+            if (lastNumber != "00000") {
+            
+             if (long.TryParse(test, out numberForAppending)) {
+                
+                    switch (lastNumberLen)
+                    {
+                        case 1:
+                            autoNumber = "0000" + (numberForAppending + 1);
+                            break;
+                        case 2:
+                            autoNumber = "000" + (numberForAppending + 1);;
+                            break;
+                        case 3:
+                            autoNumber = "00" + (numberForAppending + 1);;
+                            break;
+                        case 4:
+                            autoNumber = "0" + (numberForAppending + 1);;
+                            break;
+                        case 5:
+                            autoNumber = (numberForAppending + 1).ToString();
+                            break;
+                        default:
+                            //autoNumber = numberForAppending + 1;
+                            break;
+                    }
+                }
+             if (prefix != "") {
+                 return prefix + "-" + autoNumber.ToString();
+             }
+             return autoNumber.ToString();     
+            }
+            //first instance of the record
+            if (prefix != "") {
+                return prefix + "-" + "00001";
+            }
+            return "00001";
+            
+        }
+        
+    }
+}
